019955a8865fa80f3c819dd716d0195ae2a473ce
Jeroen Valcke <jeroen.valcke@studiohyperdrive.be>
2021-09-27 17:01:53 +0200

    feat(ctTab): only show tab when CT is of type 'content-types'

--- public/index.tsx
+++ public/index.tsx
@@ -14,7 +14,8 @@ registerCTDetailTab(CONFIG.name, {
 	module: CONFIG.module,
 	component: ContentTypeDetailTab,
 	containerId: 'update' as any,
-});
+	show: (context: any) => context.ctType === 'content-types',
+} as any);
 
 registerSiteUpdateTab(CONFIG.name, {
 	label: 'Voorvertoning',
