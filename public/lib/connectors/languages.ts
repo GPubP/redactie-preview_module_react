8194c8ce15f82c511a30a5ab077592ed104429fe
Lotje <lotte-moors@hotmail.com>
2022-03-30 09:05:39 +0200

    feat: add multilanguage to site url

---
+++ public/lib/connectors/languages.ts
@@ -0,0 +1,23 @@
+import { LanguagesModuleAPI } from '@redactie/language-module';
+import Core from '@redactie/redactie-core';
+import { first } from 'rxjs/operators';
+
+class LanguagesConnector {
+	public static apiName = 'languages-module';
+
+	public initialized$ = Core.modules
+		.selectModuleAPI<LanguagesModuleAPI>(LanguagesConnector.apiName)
+		.pipe(first());
+
+	public getApi(): LanguagesModuleAPI {
+		return Core.modules.getModuleAPI<LanguagesModuleAPI>(LanguagesConnector.apiName);
+	}
+
+	public get hooks(): LanguagesModuleAPI['hooks'] {
+		return this.getApi().hooks;
+	}
+}
+
+const languagesConnector = new LanguagesConnector();
+
+export default languagesConnector;
