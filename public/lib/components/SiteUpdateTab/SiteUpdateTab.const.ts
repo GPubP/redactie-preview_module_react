84a8f710ec1dc653c85a5bfbf025317c2e266d54
Pieterjan Van Saet <pieterjan.vansaet@studiohyperdrive.be>
2022-04-05 09:01:42 +0200

    feat: add validation

--- public/lib/components/SiteUpdateTab/SiteUpdateTab.const.ts
+++ public/lib/components/SiteUpdateTab/SiteUpdateTab.const.ts
@@ -1,3 +1,16 @@
+import { LanguageSchema } from '@redactie/language-module';
+import { MultilanguageYup } from '@redactie/utils';
+
+export const FORM_VALIDATION_SCHEMA = (languages: LanguageSchema[]): typeof MultilanguageYup =>
+	MultilanguageYup.object().shape({
+		baseUrl: MultilanguageYup.object().validateMultiLanguage(
+			languages,
+			MultilanguageYup.string()
+				.required('Url is een verplicht veld')
+				.url('Url moet valid zijn')
+		),
+	});
+
 export const PREVIEW_OPTIONS = [
 	{ key: 'not-allowed', label: 'Niet toegestaan', value: false },
 	{ key: 'allowed', label: 'Toegestaan', value: true },
